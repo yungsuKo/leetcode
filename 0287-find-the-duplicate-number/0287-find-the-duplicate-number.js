8ad2b7bdb91996c616d8c5f85bd615ef61d32993
YUNGSU KO <50985310+yungsuKo@users.noreply.github.com>
2024-03-24 23:14:23 +0900

    Time: 199 ms (9.66%), Space: 61.5 MB (49.60%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    nums.sort((a,b)=> a-b)
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i]==nums[i+1]) return nums[i]
+    }
+};
