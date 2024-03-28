ebe67c26677c1a4d56180bb845c58670ed1ae9fc
YUNGSU KO <50985310+yungsuKo@users.noreply.github.com>
2024-03-28 20:20:30 +0900

    Time: 1161 ms (11.59%), Space: 64.9 MB (5.12%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function(nums, k) {
+    if (k <= 1) return 0;
+
+  let left = 0, right = 0, product = 1, count = 0;
+  const n = nums.length;
+
+  while (right < n) {
+    product *= nums[right];
+    while (product >= k) product /= nums[left++];
+    console.log(left, right)
+    count += 1 + (right - left);
+    right++;
+  }
+
+  return count;
+    
+    
+};
