e3de019abea8dcda36a9ff1f9a1b5a6422c4084e
YUNGSU KO <50985310+yungsuKo@users.noreply.github.com>
2024-04-02 20:52:23 +0900

    Time: 55 ms (83.97%), Space: 50.3 MB (69.26%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -6,7 +6,18 @@
 var isIsomorphic = function(s, t) {
     const dict = {};
     const visited = {};
-
+    // 일단 초기에 2가지 경우의 수를 고려해야함.
+    // 첫번째로 이전에 검증한 내역에 존재하는 내역인지
+    // 여기서 만약에 이전에 존재하는 내역이라면, 검증했던 문자와 동일해야함.
+    // 만약 이전에 존재하지 않는 내역이라면, 동일한 문자가 존재하면 안됨.
+    
+    // 즉 A라는 세트와 B라는 세트가 있을 때,
+    // dict에 A[0]이라는 자리값을 만들고 B[0]을 넣음.
+    // 그리고 B[0]을 visited에 자리값으로 넣음.
+    // dict에 A[n]이라는 자리값을 만들기 이전에 dict에 동일한 자리값이 존재하는지 확인.
+    // dict에 동일한 자리값이 있다면, B[n]과 dict[A[n]]이 동일한지를 확인. 다르면 false임.
+    // 만약 dict에 자리값이 없다면 B[n]은 visited에 이력이 있으면 안됨. 있으면 false임.
+    
     for (let i = 0; i < s.length; i++) {
         const charS = s[i];
         const charT = t[i];
