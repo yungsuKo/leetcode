1c1e7d8393627fddfd221c1b013c5e519ae8c40d
YUNGSU KO <50985310+yungsuKo@users.noreply.github.com>
2024-05-18 10:17:47 +0900

    Time: 42 ms (99.19%), Space: 51.7 MB (26.48%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    if(!root) return 0;
+    let result = 0;
+    if(root.left){
+        if(!root.left.left && !root.left.right){
+            result += root.left.val;
+        }else{
+            result += sumOfLeftLeaves(root.left)
+        }
+    }
+    result += sumOfLeftLeaves(root.right)
+    
+    return result;
+};
