0e72b1d1a40d509735bfc77086d2cd785b567cea
YUNGSU KO <50985310+yungsuKo@users.noreply.github.com>
2024-05-24 09:42:48 +0900

    Time: 59 ms (38.56%), Space: 50.9 MB (31.36%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.js
+++ 2129-capitalize-the-title/2129-capitalize-the-title.js
@@ -3,15 +3,12 @@
  * @return {string}
  */
 var capitalizeTitle = function(title) {
-    const splitTitle = title.split(' ');
-    let i = 0;
-    for(let split of splitTitle){
+    const splitTitle = title.split(' ').map((split) => {
         if(split.length <= 2){
-            splitTitle[i]=split.toLowerCase();
+            return split.toLowerCase();
         }else{
-            splitTitle[i]=split.slice(0,1).toUpperCase() + split.slice(1).toLowerCase()
+            return split.slice(0,1).toUpperCase() + split.slice(1).toLowerCase()
         }
-        i++;
-    }
+    });
     return splitTitle.join(' ')
 };
