e996946ea358bbe941d291262117ba2421a863d3
YUNGSU KO <50985310+yungsuKo@users.noreply.github.com>
2024-06-07 08:08:16 +0900

    Time: 115 ms (40.24%), Space: 60.5 MB (41.87%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.js
+++ 0846-hand-of-straights/0846-hand-of-straights.js
@@ -4,17 +4,42 @@
  * @return {boolean}
  */
 var isNStraightHand = function(hand, groupSize) {
-    const groupNum = hand.length / groupSize;
-    if (hand.length === 1) return true;
+    // const groupNum = hand.length / groupSize;
+    // if (hand.length === 1) return true;
+    // if (hand.length % groupSize !== 0) return false;
+    // for(let j = 0; j < groupNum; j++) {
+    //     let min = Math.min(...hand);
+    //     for(let i = 0; i < groupSize; i++) {
+    //         let index = hand.indexOf(min + i);
+    //         if(index === -1) return false;
+    //         hand.splice(index, 1);
+    //     }
+    // }
+    // return true;
+    
     if (hand.length % groupSize !== 0) return false;
-    for(let j = 0; j < groupNum; j++) {
-        let min = Math.min(...hand);
-        for(let i = 0; i < groupSize; i++) {
-            let index = hand.indexOf(min + i);
-            if(index === -1) return false;
-            hand.splice(index, 1);
+    const count = {};
+    for(let item of hand){
+        if(!count[item]){
+            count[item] = 1
+        }else{
+            count[item] = count[item]+1;
         }
     }
-    return true;
     
+    hand.sort((a,b) => a-b);
+    
+    for(let item of hand){
+        if(count[item] == 0){
+            continue;
+        }
+        for(let i = 0; i<groupSize; i++){
+            const num = item+i;
+            if(count[num] == undefined || count[num] == 0){
+                return false;
+            }
+            count[num] = count[num] - 1
+        }
+    }
+    return true
 };
