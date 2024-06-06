48fbb22353b5c0f8eecc0f9533db3abbad279f8a
YUNGSU KO <50985310+yungsuKo@users.noreply.github.com>
2024-06-07 01:24:07 +0900

    Time: 313 ms (8.98%), Space: 53.7 MB (97.96%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} hand
+ * @param {number} groupSize
+ * @return {boolean}
+ */
+var isNStraightHand = function(hand, groupSize) {
+    const groupNum = hand.length / groupSize;
+    if (hand.length === 1) return true;
+    if (hand.length % groupSize !== 0) return false;
+    for(let j = 0; j < groupNum; j++) {
+        let min = Math.min(...hand);
+        for(let i = 0; i < groupSize; i++) {
+            let index = hand.indexOf(min + i);
+            if(index === -1) return false;
+            hand.splice(index, 1);
+        }
+    }
+    return true;
+    
+};
