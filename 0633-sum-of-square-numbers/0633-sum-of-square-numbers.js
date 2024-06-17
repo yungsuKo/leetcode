dd9ce2ce395a95f74b48307928cd6a1ce70bc74e
YUNGSU KO <50985310+yungsuKo@users.noreply.github.com>
2024-06-17 16:19:35 +0900

    Time: 52 ms (89.04%), Space: 49.9 MB (52.74%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
@@ -3,7 +3,6 @@
  * @return {boolean}
  */
 var judgeSquareSum = function(c) {
-    if(c < 0) return false;
     for(let a = 0; a*a <= c; a++){
         let b = Math.sqrt(c-a*a);
         if(b == Math.floor(b)) return true;
